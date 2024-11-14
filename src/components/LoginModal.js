448c1baa42bb000edb53cd4d083c8e6a2c5346bf
leehah <leehah0908@naver.com>
2024-11-14 17:20:42 +0900

    소셜 로그인 완벽히 끝!

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -2,6 +2,9 @@ import React, { useContext, useState } from 'react';
 import { TextField, Button, Dialog, DialogTitle, DialogContent, Box } from '@mui/material';
 import axios from 'axios';
 import AuthContext from '../contexts/UserContext';
+import kakaoLogo from '../assets/logos/kakao_logo.png';
+import naverLogo from '../assets/logos/naver_logo.png';
+import googleLogo from '../assets/logos/google_logo.png';
 
 const LoginModal = ({ open, onClose }) => {
     const { onLogin } = useContext(AuthContext);
@@ -36,6 +39,12 @@ const LoginModal = ({ open, onClose }) => {
         onClose();
     };
 
+    const handleGoogleLogin = async () => {
+        window.location.href = `${process.env.REACT_APP_BASE_URL}/social_login/google`;
+        onLogin();
+        onClose();
+    };
+
     return (
         <Dialog open={open} onClose={onClose}>
             <DialogTitle sx={{ color: '#0d41e1', paddingBottom: '0', textAlign: 'center', fontSize: '1.8rem' }}>
@@ -60,19 +69,71 @@ const LoginModal = ({ open, onClose }) => {
                 {errorMessage && <Box sx={{ color: 'red', mt: 1 }}>{errorMessage}</Box>}
 
                 {/* 로그인 버튼 */}
-                <Button fullWidth variant='contained' sx={{ mt: 2, bgcolor: '#0d41e1' }} onClick={handleLogin}>
-                    로그인
+                <Button
+                    fullWidth
+                    variant='contained'
+                    sx={{
+                        fontSize: '1rem',
+                        mt: 2,
+                        bgcolor: '#0d41e1',
+                        borderRadius: '12px',
+                        textTransform: 'none',
+                        fontWeight: 'bold',
+                    }}
+                    onClick={handleLogin}
+                >
+                    SpotOn 로그인
                 </Button>
 
                 {/* 세로로 정렬된 소셜 로그인 버튼 */}
                 <Box display='flex' flexDirection='column' gap={2} mt={2}>
-                    <Button variant='outlined' color='primary' onClick={handleKakaoLogin}>
+                    <Button
+                        variant='outlined'
+                        color='primary'
+                        onClick={handleKakaoLogin}
+                        startIcon={<img src={kakaoLogo} alt='Kakao Logo' style={{ width: 25, height: 25, marginRight: 3 }} />}
+                        sx={{
+                            backgroundColor: '#FEE500',
+                            color: '#000000',
+                            fontWeight: 'bold',
+                            fontSize: '1rem',
+                            border: 'none',
+                            borderRadius: '12px',
+                        }}
+                    >
                         카카오 로그인
                     </Button>
-                    <Button variant='outlined' color='primary' onClick={handleNaverLogin}>
+
+                    <Button
+                        variant='outlined'
+                        color='primary'
+                        onClick={handleNaverLogin}
+                        startIcon={<img src={naverLogo} alt='Naver Logo' style={{ width: 25, height: 25, marginRight: 3 }} />}
+                        sx={{
+                            backgroundColor: '#03C75A',
+                            color: '#FFFFFF',
+                            fontWeight: 'bold',
+                            fontSize: '1rem',
+                            border: 'none',
+                            borderRadius: '12px',
+                        }}
+                    >
                         네이버 로그인
                     </Button>
-                    <Button variant='outlined' color='primary'>
+                    <Button
+                        variant='outlined'
+                        color='primary'
+                        onClick={handleGoogleLogin}
+                        startIcon={<img src={googleLogo} alt='Naver Logo' style={{ width: 25, height: 25, marginRight: 15 }} />}
+                        sx={{
+                            backgroundColor: '#FFFFFF',
+                            color: '#000000',
+                            fontWeight: 'bold',
+                            fontSize: '1rem',
+                            border: '1px solid #D1D1D1',
+                            borderRadius: '12px',
+                        }}
+                    >
                         구글 로그인
                     </Button>
                 </Box>
