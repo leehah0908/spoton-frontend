fbdea013dc209ed1e8415c6efdeb4eeb049e3206
leehah <leehah0908@naver.com>
2025-01-21 10:26:03 +0900

    채팅방 메세지 폰트 변경(기존은 두꺼워서 가독성 저하)

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -2,7 +2,7 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     typography: {
-        fontFamily: '"Do Hyeon", sans-serif',
+        fontFamily: '"Do Hyeon", "Noto Sans Korean", sans-serif',
         button: {
             letterSpacing: '0.1em',
         },
